736df690e8d945705ed879e74762f12d5a3b2b04
Barnabas Edubio <barny.edubio@gmail.com>
2023-04-06 02:15:50 +0200

    basic functionality and ui done

---
+++ injectedStyle.js
@@ -0,0 +1,34 @@
+// this style is injected into and removed from the DOM depending on whether
+// the scrollbar exists in the popup window. Since the scrollbar takes up space at 
+// the side which cant be used for styling, we simply remove it entirely if 
+// we do not need it.
+export const STYLES = `
+#soundmarks_list {
+	overflow-y: scroll;
+}
+.soundmarkListItem {
+	width: calc(28rem - 9px) !important;
+}
+/* firefox scrollbar*/
+* {
+  scrollbar-width: thin;
+  scrollbar-color: rbga(0, 0, 0, 0.5) transparent;
+}
+
+/* chromium scrollbar*/
+::-webkit-scrollbar {
+  width: 8px;
+  left: -20px;
+}
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.5);
+  padding-right: 1rem;
+  border-radius: 2rem;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #c14000;
+}
+`
