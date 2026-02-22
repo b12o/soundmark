001db4beba5aceaaad804104efd9bed2f3283e85
b12o <barny@edubio.de>
2026-02-22 21:57:02 +0100

    formatting

--- content.js
+++ content.js
@@ -1,18 +1,20 @@
-const browserAPI = typeof browser !== "undefined" ? browser : chrome
+const browserAPI = typeof browser !== "undefined" ? browser : chrome;
 
-browserAPI.runtime.onMessage.addListener(
-  (request, _, sendResponse) => {
-    if (request.message === "getTrackInfo") {
-      const [trackInfo] = document.getElementsByClassName("playbackSoundBadge__titleLink")
-      const [timeStamp] = document.getElementsByClassName("playbackTimeline__timePassed")
-      sendResponse({
-        message: {
-          trackTitle: trackInfo.title,
-          trackLink: trackInfo.href.split('?')[0],
-          timeStamp: timeStamp.lastChild.innerHTML,
-        },
-        target: "popup.js"
-      })
-    }
-  })
-
+browserAPI.runtime.onMessage.addListener((request, _, sendResponse) => {
+  if (request.message === "getTrackInfo") {
+    const [trackInfo] = document.getElementsByClassName(
+      "playbackSoundBadge__titleLink",
+    );
+    const [timeStamp] = document.getElementsByClassName(
+      "playbackTimeline__timePassed",
+    );
+    sendResponse({
+      message: {
+        trackTitle: trackInfo.title,
+        trackLink: trackInfo.href.split("?")[0],
+        timeStamp: timeStamp.lastChild.innerHTML,
+      },
+      target: "popup.js",
+    });
+  }
+});
